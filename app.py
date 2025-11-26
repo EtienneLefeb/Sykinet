2b06498c4178ca8eaf7853e04abebc5c1a1734a4
EtienneLefeb <lefebvre.eti@gmail.com>
2025-11-26 21:23:48 +0100

    Application map finistère

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+from st_files_connection import FilesConnection
+import geopandas as gpd
 
-st.write("Etienne branche Kindak et Sylviane O_o")
+st.title("Carte du Finistère")
+gdf = gpd.read_file(
+    "streamlit-sykinet://base sykinet/Dept_29.zip!VECTEUR",
+    storage_options={"token": "C:/Users/lefeb/OneDrive/Documents/Sykinet/base sykinet/sound-bee-479419-k3-89105ea1b821.json"}
+)
+if gdf.crs != "EPSG:4326":
+    gdf = gdf.to_crs("EPSG:4326")
+
+st.map(gdf)
