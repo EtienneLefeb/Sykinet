71b85bb7e4700f1451ed5ab6474e10d61ed7bfc5
EtienneLefeb <lefebvre.eti@gmail.com>
2025-11-26 21:49:12 +0100

    essai map finistere

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import streamlit as st
-from st_files_connection import FilesConnection
 import geopandas as gpd
+import folium
+from streamlit_folium import st_folium
+from shapely import wkt
+from st_files_connection import FilesConnection
 
 st.title("Carte du Finistère")
-gdf = gpd.read_file(
-    "streamlit-sykinet://base sykinet/Dept_29.zip!VECTEUR",
-    storage_options={"token": "C:/Users/lefeb/OneDrive/Documents/Sykinet/base sykinet/sound-bee-479419-k3-89105ea1b821.json"}
-)
-if gdf.crs != "EPSG:4326":
-    gdf = gdf.to_crs("EPSG:4326")
+
+# Chemin vers le fichier GCS
+conn = st.connection('gcs', type=FilesConnection)
+
+df = conn.read(" streamlit-sykinet/base sykinet/base_innondation.csv", input_format="csv", ttl=600)
+
+df['geometry'] = df['geom'].apply(wkt.loads)
+
+gdf = gpd.GeoDataFrame(df, geometry='geometry', crs="EPSG:4326")
 
 st.map(gdf)
+
+
