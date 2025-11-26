60a1d9adae9c1133604ac58dcfac34b05541b523
EtienneLefeb <lefebvre.eti@gmail.com>
2025-11-26 22:56:15 +0100

    essai 10

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import streamlit as st
+from st_files_connection import FilesConnection
 import pandas as pd
 import geopandas as gpd
 from shapely import wkt
+import folium
+from streamlit_folium import st_folium
 import pydeck as pdk
 
-st.title("Carte du Finistère - légende par gridcode")
+st.title("Carte du Finistère")
 
-# Exemple : CSV déjà lu
-df = pd.read_csv("base_innondation.csv")
+# Connexion GCS via Streamlit
+conn = st.connection("gcs", type=FilesConnection)
+
+# Lire le CSV depuis le bucket GCS
+df = conn.read("streamlit-sykinet/base sykinet/base_innondation.csv",
+               input_format="csv",
+               ttl=600) 
+
+# Convertir la colonne WKT en géométrie
 df['geometry'] = df['geometry'].apply(wkt.loads)
 gdf = gpd.GeoDataFrame(df, geometry='geometry', crs="EPSG:4326")
 
